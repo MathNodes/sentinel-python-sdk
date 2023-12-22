b0f788b68d2b6175a621d9a83563c80329114821
NAST0R <92294244+NAST0R@users.noreply.github.com>
2023-12-22 12:24:14 +0100

    Delete src/src directory

--- src/src/sdk.py
+++
@@ -1,8 +0,0 @@
-import grpc
-import json
-from sentinel_sdk.querier.multiquerier import SentinelQuerier
-
-class SentinelSDK:
-    def __init__(self, grpcaddr):
-        self._channel = grpc.insecure_channel(grpcaddr)
-        self.multiquerier = SentinelQuerier(self._channel)
