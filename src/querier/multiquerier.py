31633aa9b1c055884a778f2cec3e1f2a66fd2fd3
NAST0R <92294244+NAST0R@users.noreply.github.com>
2023-12-22 12:25:29 +0100

    Completed queriers for multiquerier member of SDK
    Queriers are now accessible from multiquerier member of SDK class, as in sdk.multiquerier.querier_module.method()

--- src/querier/multiquerier.py
+++ src/querier/multiquerier.py
@@ -2,6 +2,9 @@ from sentinel_sdk.querier.modules.deposit import DepositQuerier
 from sentinel_sdk.querier.modules.node import NodeQuerier
 from sentinel_sdk.querier.modules.plan import PlanQuerier
 from sentinel_sdk.querier.modules.provider import ProviderQuerier
+from sentinel_sdk.querier.modules.session import SessionQuerier
+from sentinel_sdk.querier.modules.subscription import SubscriptionQuerier
+from sentinel_sdk.querier.modules.swap import SwapQuerier
 
 class SentinelQuerier:
     def __init__(self, channel):
@@ -10,3 +13,6 @@ class SentinelQuerier:
         self.deposit_querier = DepositQuerier(self._channel)
         self.plan_querier = PlanQuerier(self._channel)
         self.provider_querier = ProviderQuerier(self._channel)
+        self.session_querier = SessionQuerier(self._channel)
+        self.subscription_querier = SubscriptionQuerier(self._channel)
+        self.swap_querier = SwapQuerier(self._channel)
