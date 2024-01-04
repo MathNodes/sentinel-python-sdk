81c714034e5602389e6fe5712f4a9f37487e916d
Alessandro Maggio <alex.tkd.alex@gmail.com>
2024-01-04 15:12:16 +0100

    update connection test file with sdk.modules

--- test/connect.sdk.py
+++ test/connect.sdk.py
@@ -5,7 +5,8 @@ from sentinel_protobuf.sentinel.node.v2.msg_pb2 import MsgSubscribeRequest
 from sentinel_protobuf.sentinel.session.v2.msg_pb2 import MsgEndRequest, MsgStartRequest
 
 from sentinel_sdk.sdk import SDKInstance
-from sentinel_sdk.types import NodeType, TxParams
+from sentinel_sdk.types import NodeType, TxParams, Status
+from sentinel_sdk.utils import search_attribute
 
 # GRPC_VERBOSITY=debug GRPC_TRACE=tcp,http python test/queryall.py
 
@@ -13,19 +14,25 @@ from sentinel_sdk.types import NodeType, TxParams
 secret = "abdc efgh 1234 5678"
 
 sdk = SDKInstance(grpcaddr="grpc.sentinel.co", grpcport=9090, secret=secret)
+
+# The address can be founded in every modules because is inherited
+# For example: sdk.sessions._account.address
+print(sdk.nodes._account.address)
+
 node_address = "sentnode1234abcd"
-node = sdk.multiquerier.node_querier.QueryNode(node_address)
-node_status = json.loads(sdk.multiquerier.node_querier.QueryNodeStatus(node))
+node = sdk.nodes.QueryNode(node_address)
+node_status = json.loads(sdk.nodes.QueryNodeStatus(node))
+assert node.address == node_address == node_status["result"]["address"]
+
 
 try:
     # tx = sdk.transactor.subscribe_to_gigabytes(node_address=node_address, gigabytes=1)
-    tx = sdk.transactor.PrepareOrTransactMsg(
-        MsgSubscribeRequest,
+    tx = sdk.nodes.SubscribeToNode(
         node_address=node_address,
         gigabytes=1,
         hours=0,
         denom="udvpn",  # Comment this, raise exception
-        transact=True,
+        tx_params=TxParams(),
     )
 except grpc.RpcError as rpc_error:
     # TODO: this should be handled on skd side
@@ -33,13 +40,15 @@ except grpc.RpcError as rpc_error:
     print(rpc_error.details())  # pylint: disable=no-member
     print(rpc_error.debug_error_string())  # pylint: disable=no-member
 
-tx_response = sdk.transactor.wait_transaction(tx["hash"])
-subscription_id = sdk.transactor.search_attribute(
+# The wait_transaction can be founded in every modules because is inherited
+# For example: tx_response = sdk.sessions.wait_transaction(tx["hash"])
+tx_response = sdk.nodes.wait_transaction(tx["hash"])
+subscription_id = search_attribute(
     tx_response, "sentinel.node.v2.EventCreateSubscription", "id"
 )
 
 # Double verify the subscription
-subscription = sdk.multiquerier.subscription_querier.QuerySubscription(
+subscription = sdk.subscriptions.QuerySubscription(
     int(subscription_id)
 )
 assert (
@@ -47,9 +56,13 @@ assert (
     and subscription.node_address == node_address
 )
 
-sessions = sdk.multiquerier.session_querier.QuerySessionsForSubscription(
+sessions = sdk.sessions.QuerySessionsForSubscription(
     subscription.base.id
 )
+
+# TODO: return message instead of executing the tx It's currently not supported ...
+
+"""
 messages = []
 if sessions != []:
     for session in sessions:
@@ -74,20 +87,25 @@ except grpc.RpcError as rpc_error:
     print(rpc_error.code())  # pylint: disable=no-member
     print(rpc_error.details())  # pylint: disable=no-member
     print(rpc_error.debug_error_string())  # pylint: disable=no-member
+"""
 
-session_id = sdk.transactor.search_attribute(
+for session in sessions:
+    if session.status == Status.ACTIVE.value:
+        tx = sdk.sessions.EndSession(id=session.id, rating=0)
+        print(sdk.sessions.wait_transaction(tx["hash"]))
+
+tx = sdk.sessions.StartSession(id=int(subscription_id), address=node_address)
+tx_response = sdk.sessions.wait_transaction(tx["hash"])
+
+session_id = search_attribute(
     tx_response, "sentinel.session.v2.EventStart", "id"
 )
 # Double verify the session
-session = sdk.multiquerier.session_querier.QuerySession(int(session_id))
+session = sdk.sessions.QuerySession(int(session_id))
 assert (
     session.id == int(session_id)
     and session.node_address == node_address
     and session.subscription_id == int(subscription_id)
 )
 
-print(
-    sdk.transactor.post_session(
-        int(session_id), node.remote_url, NodeType(node_status["result"]["type"])
-    )
-)
+print(sdk.nodes.PostSession(int(session_id), node.remote_url, NodeType(node_status["result"]["type"])))
