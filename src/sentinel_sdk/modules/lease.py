3d58e2a16fb8d005d2d35f9bae3d979535da3cdf
freqnik <freqnik@protonmail.com>
2026-07-09 12:43:03 -0400

    Add Query stubs to leases module

--- src/sentinel_sdk/modules/lease.py
+++ src/sentinel_sdk/modules/lease.py
@@ -1,9 +1,8 @@
 
 import grpc
 import sentinel_protobuf.sentinel.lease.v1.lease_pb2 as lease_pb2
-import sentinel_protobuf.sentinel.lease.v1.querier_pb2 as sentinel_subscription_v2_querier_pb2
-import sentinel_protobuf.sentinel.lease.v1.querier_pb2_grpc as sentinel_subscription_v2_querier_pb2_grpc
-import sentinel_protobuf.sentinel.subscription.v2.subscription_pb2 as subscription_pb2
+import sentinel_protobuf.sentinel.lease.v1.querier_pb2 as sentinel_lease_v1_querier_pb2
+import sentinel_protobuf.sentinel.lease.v1.querier_pb2_grpc as sentinel_lease_v1_querier_pb2_grpc
 import sentinel_protobuf.sentinel.lease.v1.msg_pb2 as msg_pb2
 from sentinel_protobuf.sentinel.types.v1.price_pb2 import Price
 from sentinel_protobuf.sentinel.types.v1.renewal_pb2 import RenewalPricePolicy
@@ -14,12 +13,50 @@ from sentinel_sdk.types import PageRequest, TxParams
 
 class LeaseModule(Querier, Transactor):
     def __init__(self, channel: grpc.Channel, account, provider_account, client):
-        self.__stub = sentinel_subscription_v2_querier_pb2_grpc.QueryServiceStub(
+        self.__stub = sentinel_lease_v1_querier_pb2_grpc.QueryServiceStub(
             channel
         )
         self._account = account
         self._client = client
         self._provider_account = provider_account
+        
+    def QueryLease(self, lease_id: int) -> Any:
+        try:
+            r = self.__stub.QuerySession(
+                sentinel_lease_v1_querier_pb2.QueryLeaseRequest(id=lease_id)
+            )
+        except grpc._channel._InactiveRpcError as e:
+            print(e)
+            return None
+
+        return r.session
+        
+    def QueryLeases(self, pagination: PageRequest = None) -> list:
+        return self.QueryAll(
+            query=self.__stub.QueryLeases,
+            request=sentinel_lease_v1_querier_pb2.QueryLeasesRequest,
+            attribute="leases",
+            pagination=pagination,
+        )
+        
+    def QueryNodeLeases(self, str: address, pagination: PageRequest = None) -> List:
+        return self.QueryAll(
+            query=self.__stub.QueryLeasesForNode,
+            request=sentinel_lease_v1_querier_pb2.QueryLeasesForNodeRequest,
+            attribute="leases",
+            address=address,
+            pagination=pagination,
+        )
+    
+    def QueryProviderLeases(self, str: address, pagination: PageRequest = None) -> List:
+        return self.QueryAll(
+            query=self.__stub.QueryLeasesForProvider,
+            request=sentinel_lease_v1_querier_pb2.QueryLeasesForProviderRequest,
+            attribute="leases",
+            address=address,
+            pagination=pagination,
+        )
+        
         
     def EndLease(self, subscription_id: int, tx_params: TxParams = TxParams()):
         msg = msg_pb2.MsgEndLeaseRequest(
